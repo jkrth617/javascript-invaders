464c1bc42c89f64bea49c7efdd83c1b138db0aee
jkrth617 <jkrth617@gmail.com>
2015-11-18 18:00:09 -0500

    got the ship to move and shoot, no bound checking on ship movement or bullet movement

--- js/game.js
+++ js/game.js
@@ -5,12 +5,16 @@ $(document).on('ready', function(e) {
     var $ship = $('#space-ship');
     switch(e.which) {
       case 37: // left
-        moveShip($ship, -40)
+        moveShip($ship, -40);
       break;
 
       case 39: // right
-        moveShip($ship, 40)
+        moveShip($ship, 40);
       break;
+
+      case 32:
+        shipShoot($ship);
+      break
 
       default: return; // exit this handler for other keys
     }
@@ -18,8 +22,32 @@ $(document).on('ready', function(e) {
 
 })
 
+var counter = 0;
+
+var Bullet = function Bullet(ship) {
+  var startingPosition = ship.position();
+  this.xPosition = startingPosition.left;
+  this.yPosition = startingPosition.top;
+  counter ++;
+  ship.before("<div id = 'bullet"+counter+"'class='bullet'>.</div>");
+  this.id = "bullet"+counter;
+};
+
+var shipShoot = function ($ship) {
+  console.log('bang');
+  var bullet = new Bullet($ship);
+  $('#'+bullet.id).css("left", "+="+bullet.xPosition);
+  var counter = 0;
+  while(counter < 100){//unimpeded(bullet)){
+    bullet.yPosition -= 10;
+    counter += 10;
+    debugger;
+    $('#'+bullet.id).animate({ top: bullet.yPosition });
+  } 
+
+};
+
 var moveShip = function ($target, movement) {
-  debugger;
   var pos = $target.position().left;
   if (onScreen(pos)){
     var newPos = pos + movement + "";//coerce into a string
@@ -28,5 +56,9 @@ var moveShip = function ($target, movement) {
 };
 
 var onScreen = function (position) {
+  return true;
+};
+
+var unimpeded = function (bullet) {
   return true;
 };
